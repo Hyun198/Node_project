33bea97397f2a316ef72f5ff601d237c33009217
Hyun <hwn123h@naver.com>
2023-11-10 17:40:18 +0900

    post image 진행중

--- controllers/post.js
+++ controllers/post.js
@@ -3,27 +3,19 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
-const storage = multer.memoryStorage();
 
-const upload = multer({ storage: storage });
-
-
-exports.createPost = upload.single('img'), async (req, res, next) => {
+exports.createPost = async (req, res, next) => {
     try {
         const { title, desc } = req.body;
         const newPost = new Post({
             title,
             desc,
-            img: {
-                data: req.file.buffer,
-                contentType: req.file.mimetype
-            },
             author: req.user._id
         });
 
         await newPost.save();
 
-        res.send("게시글 생성 성공");
+        res.redirect('/');
     } catch (error) {
         console.error(error);
         next(error);
--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { createPost } = require('../controllers/post');
-const { renderMain } = require('../controllers/page');
+
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
 
 
-router.get('/create', isLoggedIn);
 
 router.post('/create', createPost, isLoggedIn);
 
